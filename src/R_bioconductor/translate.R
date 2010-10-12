ed7f2b16c425ea2b4f84953f041a7de4979db754
Pjotr Prins <pjotr.public01@thebird.nl>
2010-10-12 09:48:29 -0400

    R/bioconductor example fetches command line parameters

--- src/R_bioconductor/translate.R
+++ src/R_bioconductor/translate.R
@@ -1,15 +1,32 @@
+# Sequence translation with R
+#
+# Usage:
+#
+#   env BATCH_VARS="file.fa [n]" R -q --no-save --no-restore --no-readline --slave < translate.R
+#
+# Example:
+#
+#   time  env BATCH_VARS="../../test/data/test-dna.fa 2" R -q --no-save --no-restore --no-readline --slave < translate.R > /dev/null
 
+fasta = '../../test/data/test-dna.fa'
+
+args<-strsplit(Sys.getenv('BATCH_VARS'),' ')
+fasta = args$BATCH_VARS[1]
+times = as.integer(args$BATCH_VARS[2])
+
+if (is.na(times)) { times = 1 }
 
 library(GeneR)
-fasta = '../../test/data/test-dna.fa'
 idx = indexFasta(fasta)
 lines <-readLines( paste(fasta,'.ix',sep='') )
 index <-read.table(paste(fasta,'.ix',sep='') )[,1]
 n = 0
-for (name in index) {
- readFasta (file=fasta, name = name)
- ntseq = getSeq(0)
- aaseq = strTranslate(ntseq)
- cat(">",name," (",n,")\n",aaseq,"\n",sep="")
- n = n+1
+for (i in 1:times) {
+  for (name in index) {
+   readFasta (file=fasta, name = name)
+   ntseq = getSeq(0)
+   aaseq = strTranslate(ntseq)
+   cat(">",name," (",n,")\n",aaseq,"\n",sep="")
+   n = n+1
+  }
 }
