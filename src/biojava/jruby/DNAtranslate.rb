63a92ead875464b535807b083dadfbda9a985d3e
Pjotr Prins <pjotr.public01@thebird.nl>
2010-12-19 14:51:49 +0100

    JRuby

---
+++ src/biojava/jruby/DNAtranslate.rb
@@ -0,0 +1,41 @@
+#! /usr/bin/jruby
+#
+# Sequence translation using BioJava3 
+#
+# Usage:
+#
+#   ./DNAtranslate.rb file.fa [n]
+#
+# Example
+#
+#   ./DNAtranslate.rb ../../../test/data/test-dna.fa
+#
+# Dependencies: ruby bioruby bigbio biolib
+#
+#   jruby DNAtranslate.rb ../../test/data/test-dna.fa
+
+USAGE =<<EOM
+  ruby #{__FILE__} inputfile(s)
+EOM
+
+$: << File.dirname(__FILE__)+'/lib'
+
+if ARGV.size < 1
+  print USAGE
+  exit 1
+end
+
+include Java
+
+require '../jython/lib/biojava3-core-3.0-alpha1.jar'
+ARGV.each do | fn |
+  raise "File #{fn} does not exist" if !File.exist?(fn)
+  f = java.io.File.new(fn)
+  dnas = org.biojava3.core.sequence.io.FastaReaderHelper.readFastaDNASequence(f)
+  
+  for dna in dnas.values()
+    ps = dna.getRNASequence().getProteinSequence()
+    print ps,"\n"
+  end
+end
+
