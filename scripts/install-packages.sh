5d932e11518c8428d60010158bf8dfcf74080ce1
Pjotr Prins <pjotr.public01@thebird.nl>
2011-03-31 13:03:51 +0200

    Add RLib path

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -1,6 +1,10 @@
 #! /bin/sh
 
+export R_LIBS=$HOME/R_libs
+mkdir -p $R_LIBS
+
 R -q --no-save --no-restore --no-readline --slave <<EOF
 source("http://www.bioconductor.org/biocLite.R")
 biocLite("GeneR")    
 EOF
+
